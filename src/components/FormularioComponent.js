26230a933c47cca24a736044a4de207ebe3ab8a7
Juan Quiroz <juan.quiroz1009@gmail.com>
2022-11-30 12:02:28 -0500

    form-react

--- src/components/FormularioComponent.js
+++ src/components/FormularioComponent.js
@@ -1,28 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const FormularioComponent = () => {
-    const getDataForm = (e) => {
-        let data = e.target;
+  const [user, setUser] = useState({});
+
+  const getDataForm = (e) => {
+    e.preventDefault();
+    let data = e.target;
+    let user = {
+      name: data.name.value,
+      lastname: data.last_name.value,
+      gender: data.gender.value,
+      biography: data.biography.value,
     };
+    console.log(user);
+    setUser(user);
+  };
   return (
-    <div><h1>Formularios en react</h1>
-        <form onSubmit={getDataForm}>
-            <input type="text" placeholder="Nombre"></input>
+    <div>
+      <h1>Formularios en React</h1>
+      <form onSubmit={getDataForm}>
+        <input type="text" name="name" placeholder="Nombre"></input>
 
-            <input type="text" placeholder="Apellido"></input>
+        <input type="text" name="last_name" placeholder="Apellido"></input>
 
-            <select>
-                <option value="hombre">Hombre</option>
-                <option value="mujer">Mujer</option>
-            </select>
+        <select name="gender">
+          <option value="hombre">Hombre</option>
+          <option value="mujer">Mujer</option>
+        </select>
 
-            <textarea placeholder='biografía'></textarea>
-      
-            <input type="submit" value="Enviar"></input>
+        <textarea placeholder="biografía" name="biography"></textarea>
 
-        </form>
+        <input type="submit" value="Enviar"></input>
+      </form>
+      <br />
+      {user.biography && user.biography.length >= 1 && (
+        <div className="info_user">
+          {user.name}&nbsp;{user.last_name} es un {user.gender} y su biografía
+          es esta: <p>{user.biography}</p>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default FormularioComponent
+export default FormularioComponent;
